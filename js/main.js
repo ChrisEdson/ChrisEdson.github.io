edd1232f1e881de0d8b18f81f5260fa1f54bbfe7
Chris Edson <christopher.edson@gmail.com>
2014-01-22 15:59:17 +0000

    Got scrollable profile pic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $('document').ready(function () {
 
+    $.vegas('slideshow', {
+        backgrounds:[
+        {src: "img/slider/01.jpg",fade: 1e3},
+        {src: "img/slider/02.jpg",fade: 1e3},
+        {src: "img/slider/03.jpg",fade: 1e3},
+        {src: "img/slider/04.png",fade: 1e3}
+        ]
+        })('overlay');
+
+    $('.header-slider').flexslider({
+        animation: "slide",
+        directionNav: false,
+        controlNav: false,
+        direction: "vertical",
+        slideshowSpeed: 3000,
+        animationSpeed: 500,
+        pauseOnHover:false,
+        pauseOnAction:false,
+        smoothHeight: true
+    });
+
 });
