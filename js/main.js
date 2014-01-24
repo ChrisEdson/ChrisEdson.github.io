4e44dc831ef00fff43fa068178facc169b7aa9bc
Chris Edson <christopher.edson@gmail.com>
2014-01-24 16:44:44 +0000

    Got progress bars working

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
 $('document').ready(function () {
 
-    $.vegas('slideshow', {
-        backgrounds:[
-        {src: "img/slider/01.jpg",fade: 1e3},
-        {src: "img/slider/02.jpg",fade: 1e3},
-        {src: "img/slider/03.jpg",fade: 1e3},
-        {src: "img/slider/04.png",fade: 1e3}
-        ]
-        })('overlay');
+    // Activate top slideshow
+    // $.vegas('slideshow', {
+    //     backgrounds:[
+    //     {src: "img/slider/01.jpg",fade: 1000},
+    //     {src: "img/slider/02.jpg",fade: 1000},
+    //     {src: "img/slider/03.jpg",fade: 1000},
+    //     {src: "img/slider/04.png",fade: 1000}
+    //     ]
+    //     })('overlay');
 
-    $('.header-slider').flexslider({
-        animation: "slide",
-        directionNav: false,
-        controlNav: false,
-        direction: "vertical",
-        slideshowSpeed: 3000,
-        animationSpeed: 500,
-        pauseOnHover:false,
-        pauseOnAction:false,
-        smoothHeight: true
-    });
+
+    // Activate pie charts
+    $(".chart").waypoint(function() {
+        $(this).easyPieChart({barColor: "#3498db",size: "150",easing: "easeOutBounce",onStep: function(e, i, a) {
+            $(this.el).find(".percent").text(Math.round(a))
+        }})
+    },
+    {triggerOnce: !0,offset: "bottom-in-view"});
+
+    // Activate bar charts
+    $(".progress-bar").waypoint(function() {
+        $(this).css("width", $(this).attr("aria-valuenow")+'%');
+    },
+    {triggerOnce: !0,offset: "bottom-in-view"});
 
 });
